c567762898d18b7988623ba6ab1bf3f40f9df8db
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2020-11-26 15:33:34 +0300

    Commit starter files

---
+++ src/input_buffer.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+#define USERBUF_SIZE 8191
+
+struct user_iobuf {
+  char *buf;
+  unsigned int cur;
+};
+
+struct user_iobuf *create_userbuf(size_t bufsize);
+
+void process_user_input(int fd, struct user_iobuf *userbuf,
+			void (*handle_line)(char *, void *), void *cbdata);
